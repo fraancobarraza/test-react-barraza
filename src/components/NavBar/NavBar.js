3100af750126643b75a59dc7186dedb5aa0fe35c
Franco Barraza <fraancobarraza@gmail.com>
2022-05-16 21:52:44 -0300

    JSX & transpiling

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,43 @@
+//UpperCamelCase
+import './NavBar.css'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+
+
+const NavBar = () => {
+    return (
+
+        <AppBar position="static">
+            <Toolbar>
+                <div className='navlogo'>
+                    <img src='./logo.png' alt='Logo' />
+                </div>
+                <div className='navlist'>
+                    <ul>
+                        <li>
+                            <Button color='inherit'>Inicio</Button>
+                        </li>
+                        <li>
+                            <Button color='inherit'>Shop</Button>
+                        </li>
+                        <li>
+                            <Button color='inherit'>Recetas</Button>
+                        </li>
+                        <li>
+                            <Button color='inherit'>Donde Comprar</Button>
+                        </li>
+                        <li>
+                            <Button color='inherit'>Contacto</Button>
+                        </li>
+                    </ul>
+                </div>
+                <div className='login'>
+                    <Button color="inherit">Login</Button>
+                </div>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar
