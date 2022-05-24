c1691d5cee943b063e342a836aba52ab792384f1
Franco Barraza <fraancobarraza@gmail.com>
2022-05-24 03:32:16 -0300

    Componentes 2

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,41 @@
 import './Card.css'
+import { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
+import Modal from '../Modal/Modal';
 import { Button } from '@mui/material';
 
-const CardItem = ({ image, title, price } ) => {
+const CardItem = ({ image, title, price, details, stock } ) => {
+    const[open, setOpen] = useState(false)
+    const[count, setCount] = useState(0)
+    
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const addCount = () => {
+        if(count < stock){
+            setCount(count + 1)
+        }
+    }
+
+    const removeCount = () => {
+        if(count > 0){
+            setCount(count - 1)
+        }
+    }
+
+    const addCart = () => {
+        if(count <= 1){
+            console.log(`Se agregó ${count} producto al carrito`)
+        }else{
+            console.log(`Se agregaron ${count} productos al carrito`)
+        }
+    }
+
     return(
-        <Card sx={{ minWidth: 275 }}>
+        <Card sx={{ minWidth: 275 }} className="card-container">
             <CardContent>
                 <div className="card-item">    
                     <div>
@@ -13,9 +43,25 @@ const CardItem = ({ image, title, price } ) => {
                     </div>
                     <p>{title}</p>
                     <span>$ {price}</span>
-                    <Button variant='contained'>Detalle</Button>
+                    <div className='count-item'> 
+                        <Button onClick={removeCount}>-</Button>
+                        <p>{count}</p>
+                        <Button onClick={addCount}>+</Button>
+                    </div>  
+                    <Button variant='outlined' onClick={() => setOpen (true)}>Detalle</Button>
+                    <Button variant='contained' disabled={count === 0} onClick={addCart}>Comprar</Button>
                 </div>
             </CardContent>
+            <Modal
+                open={open}
+                handleClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <h2>{title}</h2>
+                <img src={`./shop/${image}`} alt={'Producto'}/>
+                <div>{details}</div>
+            </Modal>
         </Card>
     )
 }
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -7,16 +7,16 @@ const CardList = ({title}) => {
         <h2>{title}</h2>
         <Grid container className='general-container'>
             <Grid item md={3}>
-                <CardItem title={"Cilindro Muzzarella"} price={900} image={"muzza.jpg"}/>
+                <CardItem title={"Cilindro Muzzarella"} price={900} image={"muzza.jpg"} stock={5} details={"La muzzarella posee una identidad única para todos los argentinos que con sus gustos, costumbres y usos le fueron dando una identidad única a este queso. Es elaborada con leche de vaca y como producto presente de la base alimentaria reúne a miles de personas en la mesa diaria."}/>
             </Grid>
             <Grid item md={3}>
-                <CardItem title={"Cremoso Horma"} price={740} image={"cremoso.jpg"}/>
+                <CardItem title={"Cremoso Horma"} price={740} image={"cremoso.jpg"} stock={3} details={"Temperatura de conservación: 4ºC a 8ºC. Presentación: Horma. Envasado: Al vacío. Peso: 4,20 Kg."}/>
             </Grid>
             <Grid item md={3}>
-                <CardItem title={"Barra Tybo"} price={990} image={"tybo.jpg"}/>
+                <CardItem title={"Barra Tybo"} price={990} image={"tybo.jpg"} stock={6} details={"El queso Tybo argentino en barra conserva una apariencia cremosa con un color blanco amarillento uniforme. La textura es compacta y lisa, no granulosa, tal como se percibe e la vista. La corteza es lisa y bien formada, no presenta grietas ni fisuras. Su sabor es ligeramente salado, láctico y muy dócil."}/>
             </Grid>
             <Grid item md={3}>
-                <CardItem title={"Por Salut"} price={780} image={"porsalut.jpg"}/>
+                <CardItem title={"Por Salut"} price={780} image={"porsalut.jpg"} stock={8} details={"Temperatura de conservación: 2ºC a 8ºC. Presentación: Horma. Envasado: Al vacío. Peso: 3,69 Kg."}/>
             </Grid>
         </Grid>
         </>
